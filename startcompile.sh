f8cd888089f6ff3067cac852d832e79ee83acf14
ordinary <162432464+belowzeroiq@users.noreply.github.com>
2024-06-10 03:10:03 -0400

    add gki support

--- startcompile.sh
+++ startcompile.sh
@@ -12,13 +12,25 @@ function check(){
 
 function cleanup(){
 	sudo rm -rf ${KERNEL_ROOTDIR}/out
+	echo cleanup!
 }
 
-function compile(){
-	. compile.sh
+function build() {
+    if [ "${DEVICE_DEFCONFIG}" == "gki_defconfig" ]; then
+		echo gki detected!
+		sleep 3
+		echo compiling!
+        . build_gki.sh
+    else
+		echo non-gki detected!
+		sleep 3
+		echo compiling!
+        . compile.sh
+    fi
 }
+
 
 target
 check
 cleanup
-compile
+build
